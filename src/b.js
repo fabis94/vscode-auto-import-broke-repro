e51b59ce58c3249702d0150105172a2a5bcbc8bd
Fabians <fabians.printful@gmail.com>
2019-12-09 10:44:39 +0200

    Module alias example

--- src/b.js
+++ src/b.js
@@ -1,3 +1,8 @@
 function b() {
+    // This should offer to import from: @src/a.js
     aTestFunc();
 }
+
+module.exports = {
+    b
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+require("module-alias/register"); // Register path aliases
+
+// This should offer to import from: @src/b.js
+b();
